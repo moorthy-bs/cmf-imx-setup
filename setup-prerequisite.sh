947f865dd0d8427f32d7f8477c0f55b4782df73b
Moorthy Baskar <moorthy.baskaravenkatraman-sambamoorthy@linaro.org>
2020-03-27 06:57:42 +0000

    setup-prerequisite: -f force argument to download repo
    Signed-off-by: Moorthy Baskar <moorthy.baskaravenkatraman-sambamoorthy@linaro.org>

--- setup-prerequisite.sh
+++ setup-prerequisite.sh
@@ -1,18 +1,36 @@
 #!/bin/bash -x
 
-REPO_DIR="~/bin"
+REPO_DIR="$HOME/bin"
 REPO_BIN="repo"
 
+FORCE_DOWNLOAD=0
+
+while [ $# -gt 0 ]; do
+    PARAM=`echo $1`
+    case $PARAM in
+	-f | --force)
+            FORCE_DOWNLOAD=1
+	    ;;
+        *)
+	    echo "Error: unknown argument: $PARAM"
+	    exit 1
+	    ;;
+    esac
+    shift
+done
+
+#### repo tool download
 if [ ! -d ${REPO_DIR} ]; then
     mkdir -p ${REPO_DIR}
 fi
-if [ ! -f ${REPO_DIR}/${REPO_BIN} ]; then
+if [ $FORCE_DOWNLOAD -ne 0 -o ! -f ${REPO_DIR}/${REPO_BIN} ]; then
     curl https://storage.googleapis.com/git-repo-downloads/repo > ${REPO_DIR}/${REPO_BIN}
     chmod a+x ${REPO_DIR}/${REPO_BIN}
 fi
 
 export PATH=$PATH:${REPO_DIR}
 
+#### installation of prerequisite packages
 sudo apt-get update
 
 sudo apt-get install build-essential
